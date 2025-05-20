9f1ab2deb95661f9536915a8ff96bb9a4f2cb089
Chaitanya <chaitanya@trackonomysystems.com>
2025-05-20 15:29:46 -0700

    chore: corrected package budling with tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-    entry: ['src/index.ts'],
-    format: ['cjs', 'esm'],
-    dts: true,
+    entry: ['src/index.ts'], // entry point
+    format: ['esm'], // only ESM
+    dts: true, // generate .d.ts
     clean: true,
+    outDir: 'dist',
+    splitting: false, // everything in one file
+    minify: false,
+    target: 'es2020',
+    bundle: true, // BUNDLE EVERYTHING
 });
